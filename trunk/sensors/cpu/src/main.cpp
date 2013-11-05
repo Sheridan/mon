f3fc45e040fd3f11c47e9fdebd37d0e6b3a75e8b
Горлов Максим <gorlov.maxim@gmail.com>
2013-11-05 20:57:05 +0400

    Небольшое начало реализации cpu

--- trunk/sensors/cpu/src/main.cpp
+++ trunk/sensors/cpu/src/main.cpp
@@ -1,12 +1,29 @@
 /* %Id% */
 #include "mon-sensor-cpu.h"
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MON_FILE_OPEN(_filename,_name) \
+    FILE *_name; \
+    if   ((_name=fopen(_filename, "r")) == NULL) \
+         { MON_PRINT_FILEOP_ERRNO(_filename, "File open failed"); } \
+    else {
+#define MON_FILE_CLOSE(_filename, _name) } if(fclose(_name)) { MON_PRINT_FILEOP_ERRNO(_filename, "File close failed"); };
+#define MON_FILE_FSCANF(_name,_template,...) fscanf(_name, _template, __VA_ARGS__);
+
 
 MON_SENSOR_BEGIN
 
+#include <unistd.h>
+
+#define MON_SENSOR_STAT_FILE "/proc/stat"
+
+unsigned int cpu_count = 0;
+
 MON_SENSOR_INITIALIZE
 {
-
+    MON_FILE_OPEN(MON_SENSOR_STAT_FILE, file_cpu_count);
+    MON_FILE_CLOSE(MON_SENSOR_STAT_FILE, file_cpu_count);
 }
 
 MON_SENSOR_IMPLEMENT_STATISTICS_FUNCTION
@@ -20,14 +37,10 @@ MON_SENSOR_IMPLEMENT_STATISTICS_FUNCTION
 
 MON_SENSOR_IMPLEMENT_EXEMPLARS_COUNT_FUNCTION
 {
-  if(MON_SENSOR_REQUESTED_OBJECT_IS_NOT_SET)
-  {
-    return 0;
-  }
-  return 1;
+  return cpu_count;
 }
 
-#define MON_SENSOR_STAT_FILE "/proc/stat"
+
 MON_SENSOR_IMPLEMENT_AVIALABILITY_FUNCTION
 {
   bool avialable = (access(MON_SENSOR_STAT_FILE, R_OK ) != -1);
